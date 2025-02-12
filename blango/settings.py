152f876a918066d720ac439fa9d97aab92dba072
Adriaan Henning <henningfah@gmail.com>
2025-02-12 14:05:46 +0000

    Finish permissions

--- blango/settings.py
+++ blango/settings.py
@@ -208,9 +208,12 @@ class Dev(Configuration):
             "rest_framework.authentication.BasicAuthentication",
             "rest_framework.authentication.SessionAuthentication",
             "rest_framework.authentication.TokenAuthentication",
-        ]
+        ],
+        "DEFAULT_PERMISSION_CLASSES": [
+            "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+        ],
     }
-
+    
 class Prod(Dev):
     DEBUG = False
     SECRET_KEY = values.SecretValue()
